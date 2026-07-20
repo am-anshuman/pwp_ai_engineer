0cf0ea4bac939c7614416b2584b8c6926d309ef6
am-anshuman <anshumansahu0310@gmail.com>
2026-07-21 01:59:08 +0530

    Pydantic + JSON

---
+++ week1/day4/json_pydantic.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key not found")
+
+client = Groq(api_key=my_api_key)
+
+model = "llama-3.3-70b-versatile"
+role = "user"
+
+# structured output
+from pydantic import BaseModel
+class Ticket(BaseModel):
+    name: str
+    email: str
+    issue: str
+
+schema = Ticket.model_json_schema()
+
+response_format = {
+    "type": "json_object"
+}
+
+system_prompt = f"""
+Extract the personal information from the ticket strictly based on this schema and give a json output: {schema}
+"""
+message_system = {
+    "role": "system",
+    "content": system_prompt
+}
+
+text = "Hello my name is Anshuman. I have an iPhone which is not working at all. My address is Delhi. My email is abc@gmail.com. My contact number is 82134."
+prompt = f"""
+This is a customer ticket. Please extract the personal information from this: {text}
+"""
+message = {
+    "role": role,
+    "content": prompt
+}
+
+messages = [message_system, message]
+
+response = client.chat.completions.create(model=model, messages=messages, response_format=response_format)
+
+answer = response.choices[0].message.content
+print(answer)
+
+# read the json
+import json
+raw_json = answer
+data_file = json.loads(raw_json)
+ticket = Ticket(**data_file)
+
+print(ticket.name)
+print(ticket.email)
+print(ticket.issue)
